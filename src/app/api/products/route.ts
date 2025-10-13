4bb1a8c2fd55925f0ea66538b273d50c272b7e6e
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-13 09:13:46 +0545

    making the db

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,67 @@
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+import { productSchema } from "@/lib/validation/productSchema";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+export async function POST(request: Request) {
+  const data = await request.formData();
+
+  let validatedData;
+  try {
+    validatedData = productSchema.parse({
+      name: data.get("name"),
+      image: data.get("image"),
+      description: data.get("description"),
+      price: data.get("price"),
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        message: error,
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const filename = `${Date.now()}.${validatedData.image.name
+    .split(".")
+    .slice(-1)}`;
+
+  try {
+    const buffer = Buffer.from(await validatedData.image.arrayBuffer());
+    await writeFile(
+      path.join(process.cwd(), "public/assets", filename),
+      buffer
+    );
+  } catch (error) {
+    return Response.json(
+      { message: "filed to save in fs" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  try {
+    await db.insert(products).values({ ...validatedData, image: filename });
+  } catch (error) {
+    //todo: removing of stored image as if the opreation remains incomplete//
+    //yo man this is to be done a
+    return Response.json(
+      {
+        message: "faile to store Databse",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+
+
+  return Response.json({message:"the taske has been done "},
+    {status:200}
+  )
+}
