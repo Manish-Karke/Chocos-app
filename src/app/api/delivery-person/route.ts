37c8a4872d06660151e10d56f576192fdfa70bbc
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-15 18:52:59 +0545

    adding the warehouse

--- src/app/api/delivery-person/route.ts
+++ src/app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPerson } from "@/lib/db/schema";
+import { deliveryPerson, wareHouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validation/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
   const deliveryData = await request.json();
@@ -30,5 +31,23 @@ export async function POST(request: Request) {
     );
   } catch (error) {
     return Response.json({ message: "failed to store the delivery-person" });
+  }
+}
+
+export async function GET() {
+  try {
+    const deliveryPersonDetail = await db
+      .select()
+      .from(deliveryPerson)
+      .leftJoin(wareHouses, eq(deliveryPerson.wareHousesId, wareHouses.id))
+      .orderBy(desc(deliveryPerson.id));
+    return Response.json(deliveryPersonDetail);
+  } catch (error) {
+    return Response.json(
+      {
+        message: "failed to feteched data",
+      },
+      { status: 400 }
+    );
   }
 }
