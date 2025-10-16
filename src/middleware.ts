7b42dbc0a759f077052163706f72f0ae02219f3e
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-16 21:13:25 +0545

    adding the Oauth

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import withAuth from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token, req }) => {
+      console.log(token, token);
+
+      if (req.nextUrl.pathname.startsWith("/Admin")) {
+        return token?.role === "Admin";
+      } else {
+        return true;
+      }
+    },
+  },
+});
+
+export const config = {
+  matcher: ["/Admin(/.*)?"],
+};
