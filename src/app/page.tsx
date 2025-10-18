555b5ee26b6ffbc0dc75f9b45f16231174c724be
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-18 07:28:03 +0545

    addingthe admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default page;
