622e3697884615a1456c751b196c3e5d55e6b7d7
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-21 17:37:26 +0545

    adding UI for specialproduct

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 import Navigation from "./_components/header";
 import Hero from "./_components/hero";
+import SpecialProducts from "./_components/SpecialProducts";
 const HomePage = () => {
   return (
     <>
       <Navigation />
       <Hero/>
+      <SpecialProducts/>
     </>
   );
 };
