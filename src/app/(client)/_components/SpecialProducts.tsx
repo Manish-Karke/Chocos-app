a2d174b80018330f12ce97a5e5441b2d87780266
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-22 10:51:01 +0545

    adding the skeltion for UI

--- src/app/(client)/_components/SpecialProducts.tsx
+++ src/app/(client)/_components/SpecialProducts.tsx
@@ -5,11 +5,22 @@ import React from "react";
 const SpecialProducts = () => {
   const products = [
     { src: "/product1.jpg", alt: "product1", name: "Cadbury Dairy Milk" },
-    { src: "/product2.jpg", alt: "product2", name: "Mars Bars" },
-    { src: "/product3.jpg", alt: "product3", name: "Lindt Excellence Bar" },
-    { src: "/product2.jpg", alt: "product2", name: "Mars Bars" },
+    {
+      src: "/product2.jpg",
+      alt: "product2",
+      name: "Mars Bars",
+    },
+    {
+      src: "/product3.jpg",
+      alt: "product3",
+      name: "Lindt Excellence Bar",
+    },
+    {
+      src: "/product2.jpg",
+      alt: "product2",
+      name: "Mars Bars",
+    },
   ];
-
   return (
     <section className="mx-auto max-w-6xl px-5 py-14 md:py-20">
       <div className="flex items-center justify-center gap-5">
@@ -19,7 +30,6 @@ const SpecialProducts = () => {
         </h2>
         <Separator className="h-0.5 w-20 bg-brown-900" />
       </div>
-
       <div className="mt-20 grid grid-cols-1 gap-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product, index) => (
           <div
@@ -29,9 +39,11 @@ const SpecialProducts = () => {
             <Image
               src={product.src}
               alt={product.alt}
-              width={220}
-              height={220}
-              className="rounded-full border-8 object-cover"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "220px", height: "220px" }}
+              className="rounded-full border-8"
             />
             <p className="font-semibold text-brown-900">{product.name}</p>
           </div>
--- src/app/(client)/_components/products.tsx
+++ src/app/(client)/_components/products.tsx
@@ -7,10 +7,12 @@ import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Loader2 } from "lucide-react";
 import { Product } from "@/types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Products = () => {
+  const skeletons = Array.from({ length: 4 });
+
   const {
     data: products,
     isLoading,
@@ -21,16 +23,6 @@ const Products = () => {
     staleTime: 10 * 1000,
   });
 
-  // ✅ Show loader while fetching
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center py-20">
-        <Loader2 className="size-10 animate-spin text-brown-800" />
-      </div>
-    );
-  }
-
-  // ✅ Handle error case
   if (isError) {
     return (
       <div className="text-center py-20 text-red-600">
@@ -53,36 +45,51 @@ const Products = () => {
 
         {/* Product Grid */}
         <div className="mt-20 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {products?.map((product: Product) => (
-            <div
-              key={product.id}
-              className="flex flex-col justify-center gap-5 items-start"
-            >
-              <Image
-                src={`/assets/${product.image}`}
-                alt={product.name}
-                width={500}
-                height={500}
-                sizes="100vw"
-                className="aspect-square rounded-t-md object-cover shadow-lg hover:scale-105 transition-transform duration-300 hover:cursor-pointer"
-              />
-
-              <div className="w-full items-center">
-                <p className="text-lg font-semibold text-brown-900">
-                  {product.name}
-                </p>
-                <div className="mt-2 space-x-2">
-                  <span className="font-bold">${product.price}</span>
+          {isLoading ? (
+            <>
+              {skeletons.map((_, i) => (
+                <div key={i} className="flex h-full w-full flex-col gap-5">
+                  <Skeleton className="aspect-square w-full rounded-md bg-brown-100" />
+                  <Skeleton className="h-5 w-full rounded-md bg-brown-100" />
+                  <Skeleton className="h-5 w-10 rounded-md bg-brown-100" />
+                  <Skeleton className="h-8 w-full rounded-md bg-brown-100" />
                 </div>
+              ))}
+            </>
+          ) : (
+            <>
+              {products?.map((product: Product) => (
+                <div
+                  key={product.id}
+                  className="flex flex-col justify-center gap-5 items-start"
+                >
+                  <Image
+                    src={`/assets/${product.image}`}
+                    alt={product.name}
+                    width={500}
+                    height={500}
+                    sizes="100vw"
+                    className="aspect-square rounded-t-md object-cover shadow-lg hover:scale-105 transition-transform duration-300 hover:cursor-pointer"
+                  />
 
-                <Link href={`/product/${product.id}`} className="block">
-                  <Button className="bg-brown-800 hover:bg-brown-600 w-full mt-3">
-                    Buy Now
-                  </Button>
-                </Link>
-              </div>
-            </div>
-          ))}
+                  <div className="w-full items-center">
+                    <p className="text-lg font-semibold text-brown-900">
+                      {product.name}
+                    </p>
+                    <div className="mt-2 space-x-2">
+                      <span className="font-bold">${product.price}</span>
+                    </div>
+
+                    <Link href={`/product/${product.id}`} className="block">
+                      <Button className="bg-brown-800 hover:bg-brown-600 w-full mt-3">
+                        Buy Now
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
