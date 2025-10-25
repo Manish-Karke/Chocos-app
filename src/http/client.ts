52d230761c29b129e8f872a9a8e2cb7ed7fa3b65
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-10-25 17:36:10 +0545

    again

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "" // relative URLs for deployed app on Vercel
+    : "http://localhost:3000/api"; // local development
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
