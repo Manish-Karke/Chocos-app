8ec9bf9bb2f0f80d534cdfb1218556ea96874eda
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-11-12 15:41:13 +0545

    Refactor axios instance in client.ts
    Refactor axios instance creation for better readability.

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
-
 const baseURL ="https://my-book-store-b63l.vercel.app"
-
-export const api = axios.create({
-  baseURL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-});
+export const api = axios.create({ baseURL, withCredentials: true, 
+                                 headers: { "Content-Type": "application/json",
+                                           Accept: "application/json", 
+                                          }, 
+                                
+                                });
